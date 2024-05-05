83d556ace481f09f9d9dc38daf8fa51b4ad020ee
sabilafiridas <sabilafirdias@gmail.com>
2024-05-05 18:48:24 +0700

    Add project files.

---
+++ ucp procedural/ucp procedural.cpp
@@ -0,0 +1,59 @@
+//1. Deklarasi variabel dari class mahasiswa dan class bangundatar: string nama, int umur, float panjang.
+// 
+//2. Prosedur dan fungsi dari operasi penjumlahan:
+//  - Fungsi:
+// int penjumlahan(int a, int b) {
+// return a + b;
+// }
+//   - Prosedur:
+// void inputData() {
+// cout << "\nMasukkan bilangan pertama: ";
+// cin >> bilangan1;
+// cout << "Masukkan bilangan kedua: ";
+// cin >> bilangan2;
+// } 
+// 
+//3. Looping Do...While
+// do
+// {
+// cout << "Perulangan Do..While" << endl;
+// x = rand() % 10;
+// cout << "Nilai X : " << x << endl;
+// }
+// while (x > 4); 
+//
+// 4. Conditional statement searchNode Single Linked List:
+// if (current == NULL) {
+//      return false;
+// }
+//      else if (current->noMhs == nim) {
+//      return true;
+// }
+//      else {
+//      return false;
+// }
+//
+//5. Struct Mahasiswa:
+// struct Mahasiswa {
+//      string nim;
+//      string nama;
+//      string alamat;
+//      int umur;
+//      };
+//
+//6. Jawaban:
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    string kota;
+    string status;
+    Node* next;
+};
+
+int main()
+{
+    
+}
+
